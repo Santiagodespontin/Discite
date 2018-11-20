2236d8c85293e5211cb8982bd20e72d83c099f04
Santiago <santiagodespontin@gmail.com>
2018-11-20 15:02:16 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+// use Symfony\Component\Routing\Annotation\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','WebController@index');
+Route::get('/faq','WebController@faq');
+Route::get('/register','WebController@register');
+Route::post('/register','WebController@faq');
+Route::get('/login','WebController@login');
+Route::post('/login','WebController@faq');
